19e2df670f938e9304f1deb1cb266aa11fc241f5
ushieru <uzielcocolan@gmail.com>
2021-02-28 16:39:14 -0600

    add Tickets DB

--- src/DB/index.ts
+++ src/DB/index.ts
@@ -4,10 +4,7 @@ import * as Path from 'path';
 const getDatabasePath = (name: string) =>
     Path.join(__dirname, 'databases', `${name}.db`);
 
-const databases = {
-    users: new Datastore({ filename: getDatabasePath('users'), timestampData: true, autoload: true }),
-    products: new Datastore({ filename: getDatabasePath('products'), timestampData: true, autoload: true }),
-    ingredients: new Datastore({ filename: getDatabasePath('ingredients'), timestampData: true, autoload: true }),
-};
-
-export default databases;
+export const users = new Datastore({ filename: getDatabasePath('users'), timestampData: true, autoload: true });
+export const products = new Datastore({ filename: getDatabasePath('products'), timestampData: true, autoload: true });
+export const ingredients = new Datastore({ filename: getDatabasePath('ingredients'), timestampData: true, autoload: true });
+export const tickets = new Datastore({ filename: getDatabasePath('tickets'), timestampData: true, autoload: true });
